5599e416e1fad5a20888027626d18f66ece20226
Sri Nikhil Reddy <srinikhilreddy99@gmail.com>
2025-01-16 22:52:50 -0500

    Github workflow update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,17 +5,17 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  server: {
+    port: 3000,
+    open: true
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
   },
 })
